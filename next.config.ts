462612ef043160b1c7b1fdda7e1aa0b6dce5a734
Pedro Vilamitjana <pedrovilamitjana98@gmail.com>
2025-07-29 15:31:38 +0000

    Actúa como desarrollador front-end y reemplaza únicamente el placeholder

--- next.config.ts
+++ next.config.ts
@@ -34,6 +34,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'instagram.fros9-1.fna.fbcdn.net',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
 };
